560f5a89bda62782a649ef91cf268a9af7644af9
Abigail Chin <chinabigail08@gmail.com>
2017-04-13 18:29:15 -0700

    before bootstrap

--- demo.php
+++ demo.php
@@ -1,4 +1,9 @@
-<?php
-	phpinfo();
-?>
+<html>
+<body>
+
+Thank you for visiting my site! <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
 
